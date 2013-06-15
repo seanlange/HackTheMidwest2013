cfb770a230e094692aeb187d495f2af9fcce49df
Ian Joyce <ian.joyce@gmail.com>
2013-06-15 17:30:21 -0500

    Show latest chant.

---
+++ Scripts/louder.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    LoadLatestChant();
+    
+    window.setInterval(LoadLatestChant,5000);
+});
+
+function LoadLatestChant() {
+    $.get('/api/latestchant', function (data) {
+        console.log("loading latest chant: " + data.CHANT_NAME)
+        $('div.latest-chant').html("<b>Latest:  </b>" + data.CHANT_NAME);
+    });
+}
