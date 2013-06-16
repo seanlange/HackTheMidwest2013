6f965b701b57c37b0673834f515a55b1b91901dd
Ian Joyce <ian.joyce@gmail.com>
2013-06-15 19:22:18 -0500

    update latest chant.

---
+++ Controllers/CapoController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using KcCauldronCapo.Model;
+
+namespace KcCauldronCapo.Controllers
+{
+    public class CapoController : Controller
+    {
+        public ActionResult Index()
+        {
+            var entities = new Hackathon_TestEntities();
+            entities.Configuration.LazyLoadingEnabled = false;
+
+            var chants = entities
+                .CHANTS
+                .OrderBy(c => c.CHANT_NAME)
+                .ToList();
+
+            return View(chants);
+        }
+
+        //
+        // GET: /Capo/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Capo/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Capo/Create
+
+        [HttpPost]
+        public ActionResult Create(int chantId)
+        {
+            var entities = new Hackathon_TestEntities();
+
+            var chantHistory = new ChantHistory
+            {
+                CHANT_ID = chantId,
+                DATE_PLAYED_DT_TM = DateTime.Now,
+            };
+
+            entities.ChantHistories.Add(chantHistory);
+            entities.SaveChanges();
+
+            return new EmptyResult();
+        }
+
+        //
+        // GET: /Capo/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Capo/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Capo/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Capo/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Scripts/louder.js
+++ Scripts/louder.js
@@ -1,12 +1,55 @@
-﻿$(document).ready(function () {
+﻿var context = {
+    'current-chant-detail': 'lyrics',
+    'current-chant': undefined,
+};
+
+$(document).ready(function () {
     LoadLatestChant();
     
-    window.setInterval(LoadLatestChant,5000);
+    window.setInterval(LoadLatestChant, 5000);
+
+    $("#btn-lyrics").click(function () {
+        context["current-chant-detail"] = "lyrics";
+        $('div.chant-detail').html(context["current-chant"].LYRICS);
+    });
+    
+    $("#btn-history").click(function () {
+        context["current-chant-detail"] = "history";
+        $('div.chant-detail').html(context["current-chant"].HISTORY);
+    });
+    
+    $("#btn-errata").click(function () {
+        context["current-chant-detail"] = "errata";
+        $('div.chant-detail').html(context["current-chant"].ERRATA);
+    });
 });
 
 function LoadLatestChant() {
     $.get('/api/latestchant', function (data) {
-        console.log("loading latest chant: " + data.CHANT_NAME)
+        console.log("loading latest chant: " + data.CHANT_NAME);
+        context["current-chant"] = data;
         $('div.latest-chant').html("<b>Latest:  </b>" + data.CHANT_NAME);
+        SetChantDetail();
+    });
+}
+
+function SetChantDetail() {
+    if (context["current-chant-detail"] == "lyrics")
+        $('div.chant-detail').html(context["current-chant"].LYRICS);
+    
+    if (context["current-chant-detail"] == "history")
+        $('div.chant-detail').html(context["current-chant"].HISTORY);
+    
+    if (context["current-chant-detail"] == "errata")
+        $('div.chant-detail').html(context["current-chant"].ERRATA);
+}
+
+function SetCurrentChant(chantId) {
+    console.log("seting current chant to: " + chantId);
+    $.ajax({
+        type: "POST",
+        url: "/capo/create",
+        data: { 'chantId': chantId },
+        dataType: "json"
     });
 }
