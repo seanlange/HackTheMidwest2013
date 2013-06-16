ad1ecab0d0c11b35064c0549d08344706f0c34c4
Scott Goering <scottgoering@gmail.com>
2013-06-16 03:40:00 -0500

    Added Alert Page

--- Model/Model.Designer.cs
+++ Model/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\Ian\Documents\GitHub\HackTheMidwest2013\Model\Model.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Users\SG020544\Documents\GitHub\HackTheMidwest2013\Model\Model.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
